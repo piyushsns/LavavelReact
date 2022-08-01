09e5bc8f61f3dad8885f13a56f8579e31bbbf5a2
Piyush SNSSystem <piyush.jain@snssystem.com>
2022-08-01 12:53:51 +0530

    added changes

--- Frontend/src/redux/CreateStore.js
+++ Frontend/src/redux/CreateStore.js
@@ -7,8 +7,9 @@ export const store = createStore(
     RootReducer,
     compose(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
     ),
 );
 
--- Frontend/src/services/HttpService.js
+++ Frontend/src/services/HttpService.js
@@ -1,6 +1,6 @@
 export default class HttpService
 {
-    url = "http://localhost:8000/api";
+    url = "http://localhost:8080/api";
 
 
     postData = async(item, added_url, tokenId="") => {
